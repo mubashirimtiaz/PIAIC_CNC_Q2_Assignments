6e39ac63465ef3c65eddef9c2a47bc71c9cac2b6
mubashir_ali <mubashir.imtiaz123@gmail.com>
2020-01-08 04:34:16 +0500

    added HTML,CSS,JS asssignments

---
+++ Q2_Assignments/HTML_CSS_Assignment/Final_Assignment_of_HTML_and_CSS/panacloud_portfolio/js/main.js
@@ -0,0 +1,12 @@
+function premiumOffer(){
+    var arr = ["Unlimited pages website","1 Design Content","Turn arround bussiness days","10 stock photo","Free on page SEO","FREE CMS integration","Satisfaction Gurantee"];
+    alert(`You Ordered Premium Offer\nthat contain: \n${arr.toString()}`);
+}
+function premiumPlusOffer(){
+    var arr = ["Unlimited pages website","1 Design Content","Turn arround bussiness days","10 stock photo","Free on page SEO","FREE CMS integration","Satisfaction Gurantee"];
+    alert(`You Ordered Premium Plus Offer\nthat contain: \n${arr.toString()}`);
+}
+function basicOffer(){
+    var arr = ["Unlimited pages website","1 Design Content","Turn arround bussiness days","10 stock photo","Free on page SEO","FREE CMS integration","Satisfaction Gurantee"];
+    alert(`You Ordered Basic Offer\nthat contain: \n${arr.toString()}`);
+}
